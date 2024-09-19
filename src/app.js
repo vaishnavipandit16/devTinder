917ee794c7d497e3664a8addb9964978267e7d29
vaishnavipandit16 <mjariwala98@gmail.com>
2024-09-19 15:52:51 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
   res.send({ firstName: "Vaishnavi", lastName: "Pandit" });
 });
 
